b9255c8401a70eddff9952b262f5624f6bc6a436
Altair <eclipsealtair@gmail.com>
2019-08-21 21:31:23 +0300

    add meta

---
+++ digitalrush/sitemap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['development', 'promotion', 'prices', 'contacts', 'landing', 'card', 'catalog', 'online_store', 'seo', 'smm', 'context']
+
+    def location(self, item):
+        return reverse(item)
--- digitalrush/urls.py
+++ digitalrush/urls.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.contrib.sitemaps.views import sitemap
+from .sitemap import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    }
 
 admin.autodiscover()
 
@@ -16,6 +23,10 @@ urlpatterns = [
     path('', include('seo.urls')),
     path('', include('smm.urls')),
     path('', include('context.urls')),
+    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /admin\nSitemap: http://digitalrush.ru/sitemap.xml", content_type="text/plain")),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('manifest.json', lambda r: HttpResponse('{"name": "DigitalRush","short_name": "DigitalRush","start_url": "/",' '"display": "standalone","theme_color": "#000", "background_color": "#fff"}', content_type="application/json"))
+
 ] \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
