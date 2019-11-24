7536243f138b4cf025c96a7049e93ff4cb9123d8
EclipseAltair <eclipsealtair@yandex.ru>
2019-11-24 15:55:39 +0300

    testing comitting

--- digitalrush/dashboard.py
+++ digitalrush/dashboard.py
@@ -14,10 +14,6 @@ from grappelli.dashboard.utils import get_admin_site_name
 
 
 class CustomIndexDashboard(Dashboard):
-    """
-    Custom index dashboard for www.
-    """
-
     def init_with_context(self, context):
         site_name = get_admin_site_name(context)
 
