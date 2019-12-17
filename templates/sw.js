d93e7bbf8042b649f55925dbc7df9442ce3921c9
seradya <seradya@gmail.com>
2019-12-17 04:24:03 +0300

    add scss font-awesome

--- templates/sw.js
+++ templates/sw.js
@@ -1,10 +1,8 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            // Регистрация успешна
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function(err) {
-            // Регистрация не успешна
             console.log('ServiceWorker registration failed: ', err);
         });
     });
@@ -12,7 +10,6 @@ if ('serviceWorker' in navigator) {
 var CACHE_NAME = 'digitalrush-cache-v1';
 var urlsToCache = [
   '/',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css',
   '/static/css/style.min.css',
   '/static/js/scripts.min.js',
   '/static/js/parallax.min.js'
@@ -37,7 +34,6 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-
         /* Клонируем запрос. Запрос - это поток, может быть обработан только раз.
            Если мы хотим использовать объект request несколько раз, его нужно клонировать */
         var fetchRequest = event.request.clone();
@@ -48,15 +44,12 @@ self.addEventListener('fetch', function(event) {
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-
             // Клонируем ответ. Объект response также является потоком.
             var responseToCache = response.clone();
-
             caches.open(CACHE_NAME)
               .then(function(cache) {
                 cache.put(event.request, responseToCache);
               });
-
             return response;
           }
         );
