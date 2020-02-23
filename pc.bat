d8d8dd5916417ae96dcd231528e8fb23b77d560d
EclipseAltair <eclipsealtair@yandex.ru>
2020-02-23 16:29:59 +0300

    change hover bg-color

--- pc.bat
+++
@@ -1,4 +0,0 @@
-cd digitalrush
-source digitalrushenv/bin/activate
-git pull origin master
-python3 manage.py collectstatic
--- pk.bat
+++
@@ -1,4 +0,0 @@
-cd digitalrush
-source digitalrushenv/bin/activate
-git pull origin master
-sudo killall gunicorn
